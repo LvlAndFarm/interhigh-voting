cc552c0ee766825f6fd9e130246f20079c253372
LvlAndFarm <walid.openrealms@gmail.com>
2020-05-22 01:04:35 +0100

    Fix persistent overlay mask

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as arrive from "arrive";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,12 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Fix for presistent EuiOverlayMask
+document.arrive(".euiOverlayMask", function() {
+    // document.getElementsByClassName("euiOverlayMask").map (elem)
+    if (this.getAttribute("class")==="euiOverlayMask" && this.children.length<1) {
+        this.remove();
+        console.log(this);
+    }
+});
